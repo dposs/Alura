20de1c97e162d0d754d3d23e1508b31787582cfe
cpd_possamai <daniel.possamai@grendene.com.br>
2017-04-28 13:36:45 -0300

    20170428

--- projeto-jpa-2/src/main/java/br/com/caelum/dao/ProdutoDao.java
+++ projeto-jpa-2/src/main/java/br/com/caelum/dao/ProdutoDao.java
@@ -1,16 +1,19 @@
 package br.com.caelum.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityGraph;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import javax.persistence.criteria.Subquery;
 
 import org.hibernate.jpa.QueryHints;
 import org.springframework.stereotype.Repository;
@@ -47,35 +50,35 @@ public class ProdutoDao {
 				.getSingleResult();
 	}
 
-//	public List<Produto> getProdutos(String nome, Integer categoriaId, Integer lojaId) {
-//		CriteriaBuilder criteria = em.getCriteriaBuilder();
-//		CriteriaQuery<Produto> query = criteria.createQuery(Produto.class);
-//		Root<Produto> produto = query.from(Produto.class);
-//
-//		Path<String> nomePath = produto.get("nome");
-//		Path<Integer> lojaPath = produto.get("loja").get("id");
-//		Path<Integer> categoriaPath = produto.join("categorias").get("id");
-//
-//		List<Predicate> predicates = new ArrayList<>();
-//
-//		if (!nome.isEmpty()) {
-//			Predicate nomeIgual = criteria.like(nomePath, "%" + nome + "%");
-//			predicates.add(nomeIgual);
-//		}
-//		if (categoriaId != null) {
-//			Predicate categoriaIgual = criteria.equal(categoriaPath, categoriaId);
-//			predicates.add(categoriaIgual);
-//		}
-//		if (lojaId != null) {
-//			Predicate lojaIgual = criteria.equal(lojaPath, lojaId);
-//			predicates.add(lojaIgual);
-//		}
-//
-//		query.where(predicates.toArray(new Predicate[0]));
-//		
-//		TypedQuery<Produto> typedQuery = em.createQuery(query);
-//		return typedQuery.getResultList();
-//	}
+	public List<Produto> getProdutos(String nome, Integer categoriaId, Integer lojaId) {
+		CriteriaBuilder criteria = em.getCriteriaBuilder();
+		CriteriaQuery<Produto> query = criteria.createQuery(Produto.class);
+		Root<Produto> produto = query.from(Produto.class);
+
+		Path<String> nomePath = produto.get("nome");
+		Path<Integer> lojaPath = produto.get("loja").get("id");
+		Path<Integer> categoriaPath = produto.join("categorias").get("id");
+
+		List<Predicate> predicates = new ArrayList<>();
+
+		if (!nome.isEmpty()) {
+			Predicate nomeIgual = criteria.like(nomePath, "%" + nome + "%");
+			predicates.add(nomeIgual);
+		}
+		if (categoriaId != null) {
+			Predicate categoriaIgual = criteria.equal(categoriaPath, categoriaId);
+			predicates.add(categoriaIgual);
+		}
+		if (lojaId != null) {
+			Predicate lojaIgual = criteria.equal(lojaPath, lojaId);
+			predicates.add(lojaIgual);
+		}
+
+		query.where(predicates.toArray(new Predicate[0]));
+		
+		TypedQuery<Produto> typedQuery = em.createQuery(query);
+		return typedQuery.getResultList();
+	}
 	
 	public List<Produto> getProdutosConjunction(String nome, Integer categoriaId, Integer lojaId) {
 		EntityGraph<Produto> graph = em.createEntityGraph(Produto.class);
@@ -84,11 +87,9 @@ public class ProdutoDao {
 		CriteriaBuilder criteria = em.getCriteriaBuilder();
 		CriteriaQuery<Produto> query = criteria.createQuery(Produto.class);
 		Root<Produto> produto = query.from(Produto.class);
-		query.select(produto);
 
 		Path<String> pNomeProduto = produto.get("nome");
 		Path<Integer> pLojaId = produto.get("loja").get("id");
-		Path<Integer> pCategoriaId = produto.join("categorias", JoinType.LEFT).get("id");
 
 		Predicate where = criteria.conjunction();
 		
@@ -97,17 +98,26 @@ public class ProdutoDao {
 		}
 		
 		if (categoriaId != null) {
-			where = criteria.and(where, criteria.equal(pCategoriaId, categoriaId));
+			Subquery<Produto> subQuery = query.subquery(Produto.class); 
+			Root<Produto> subProduto = subQuery.from(Produto.class);  
+			
+			Predicate subWhere = criteria.conjunction();
+			subWhere = criteria.and(subWhere, criteria.equal(subProduto.join("categorias", JoinType.LEFT).get("id"), categoriaId));
+			subWhere = criteria.and(subWhere, criteria.equal(subProduto.get("id"), produto.get("id")));
+			
+			subQuery.select(subProduto).where(subWhere); 
+			
+			where = criteria.and(where, criteria.exists(subQuery));
 		}
 		
 		if (lojaId != null) {
 			where = criteria.and(where, criteria.equal(pLojaId, lojaId));
 		}
 
-		query.where(where);
+		query.select(produto).where(where);
 		
 		return em.createQuery(query)
-				.setHint(QueryHints.HINT_FETCHGRAPH, graph)
+				.setHint(QueryHints.HINT_LOADGRAPH, graph)
 				.getResultList();
 	}
 
